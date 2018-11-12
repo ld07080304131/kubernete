5cce723d92f36cecf0ce9a398a158c1ce640c19b
liukai <ld07080304131@163.com>
2018-11-12 15:13:00 +0800

    update build_local_nginx_ingress_images.sh

--- scripts/build_local_nginx_ingress_images.sh
+++ scripts/build_local_nginx_ingress_images.sh
@@ -3,7 +3,7 @@
 images=(defaultbackend:1.4)
 for image in ${images[@]} ; do
   docker pull index.docker.io/15810855834/$image
-  docker tag index.docker.io/15810855834/$image gcr.io/$image
+  docker tag index.docker.io/15810855834/$image k8s.gcr.io/$image
   docker rmi index.docker.io/15810855834/$image
 
 done
